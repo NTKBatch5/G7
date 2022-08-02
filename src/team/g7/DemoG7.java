6a069a05079e426402c48051615070a3bc6c98c1
qamar <qamar@HPComputer.mynetworksettings.com>
2022-08-01 22:45:30 -0400

    changes G7

--- src/team/g7/DemoG7.java
+++ src/team/g7/DemoG7.java
@@ -10,4 +10,11 @@ public class DemoG7 {
 	public static void display() {
 		System.out.println(" team G7 rocks");
 	}
+	
+public static void add(int a, int b) {
+	System.out.println(a+b);
 }
+
+	}
+	
+
