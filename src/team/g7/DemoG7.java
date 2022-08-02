224775d1a2a8becb42bc4a2bbcbe91295f1abee0
qamar <qamar@HPComputer.mynetworksettings.com>
2022-08-01 22:54:59 -0400

    implemented

--- src/team/g7/DemoG7.java
+++ src/team/g7/DemoG7.java
@@ -9,14 +9,12 @@ public class DemoG7 {
 // hello team g7
 	public static void display() {
 		System.out.println(" team G7 rocks");
-		System.out.println("*****");
-		System.out.println ("Java is fun");
 	}
 	
 public static void add(int a, int b) {
 	System.out.println(a+b);
 }
-
+//comment 
 	}
 	
 
