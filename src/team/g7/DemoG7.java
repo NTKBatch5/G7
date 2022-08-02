b2c9aa0e5a997a823a8a5c95e0ad384a18e74d1e
Sam <Sam@Brenden-PC.fios-router.home>
2022-08-01 22:24:21 -0400

    hello

--- src/team/g7/DemoG7.java
+++ src/team/g7/DemoG7.java
@@ -5,5 +5,9 @@ public class DemoG7 {
 	public static void main(String[] args) {
 		System.out.println("Test");
 	}
-
+	
+// hello team g7
+	public static void display() {
+		System.out.println(" team G7 rocks");
+	}
 }
