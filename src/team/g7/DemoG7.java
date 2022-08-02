4d42a04d69874df3d7d1df3f1a4c12a53be12c73
Sam <Sam@Brenden-PC.fios-router.home>
2022-08-01 23:29:25 -0400

    commetns more comments

--- src/team/g7/DemoG7.java
+++ src/team/g7/DemoG7.java
@@ -2,13 +2,14 @@ package team.g7;
 
 /*
  * this is a class used to test github stashes , push and pull.
- * 
+ * Qamar and Alena practices stashes, push and pull.
+ * James did too.
  * 
  * 
  */
 
 public class DemoG7 {
-	
+	// main method
 	public static void main(String[] args) {
 		System.out.println("G7 team is testing github....");
 		System.out.println("Test");
@@ -17,11 +18,11 @@ public class DemoG7 {
 // hello team g7
 	public static void display() {
 		System.out.println(" team G7 rocks");
-		
+		System.out.println("*************");
 		System.out.println("Rain Rain Go Away");
 		System.out.println ("Java is fun");
 	}
-	
+	// add method
 public static void add(int a, int b) {
 	System.out.println(a+b);
 	
