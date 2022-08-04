f16605b66b308fdf69de3ec5ee92083c29168f6b
jameslopez <jameslopez@jamess-air.lan>
2022-08-03 21:12:08 -0400

    deleted James

--- src/team/g7/DemoG7.java
+++ src/team/g7/DemoG7.java
@@ -1,64 +1,9 @@
 package team.g7;
-import java.util.Random;
-import java.util.Scanner;
-
-
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-<<<<<<< HEAD
-// trying to push
-/*
-
- * this team g7 g7 is wonderful...............................................................
- * this is a class used to test github stashes , push and pull...............................
- * Qamaaaaaaaaar and Alenaaaaaaaa practices stashes, push and pull.........................
- * James did too............................................................................
- * day2- issam: added some comments. practiced pull and push................................
- * added comment......................................................................................
- * DONT FORGET AaaaaaaDAM, ISssaaaaAM, AND ABDUuuuuuuuLMOSIN!!!!!!.............................
- * we didn't forget abduuuuuuuul kareem and ahmaaaaaaaaad..................................
- * day2- issam: added some comments. practiced pull and push.
-=======
- * this team is wonderful.....................................................
- * this is a class used to test github stashes , push and pull...............
- * Qamar and Alena practices stashes, push and pull.........................
- * James did too.............................................................
- * day2- issam: added some comments. practiced pull and push.................
- * added comment..............................................................
- * DONT FORGET ADAM, ISAAM, AND ABDULMOSIN!!!!!!.............................
- * we didn't forget abdul kareem and ahmad..................................
-
- * 
- * 8/2/22 abdulKareem practiced stash pull push ...........................
- * 8/3/22 AQamar practices some stash pull and push..........................
- *8/3/22 ahmad practiced some pull push....................................
- * hello........
- * way to go team G7 !!!!!
- */
-// hello.......................................................
-//added before abdul......
-//hello again ....
-//test.......
-//adam added comment.....
-// qamar tried to add something.....
-// new push
-
 
 
 public class DemoG7 {
 	
-	//James first push
-	//James second push
-	//James Third push
-	// main method
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) {
 		System.out.println("G7 team is testing github....");
 		System.out.println("Test");
 		
@@ -106,30 +51,7 @@ public class DemoG7 {
 		String query1 = "select * from hr.departments;";
 	}
 }
-		// STEP 1 MAKE SURE WE HAVE THE DATABASE DEPENDENCY IN THE POM.XML FILE
-
-
 	
-	
-        // check if a date is supplied is in the corect format and it is going to be a String . date should be separated by / like 7/27/2022 and first character needs to a number and within the range of 1-12
-            // and for days , it should be within 1-31 and years needs to be a number.
-            // 7 / 27 / 2022
-  
-        // check if a date is supplied is in the correct format and it is going to be a String . date should be separated by / like 7/27/2022 and first character needs to be a number and within the range of 1-12
-            //  for days , it should be within 1-31 and years needs to be a number.
-  
-	// add method
-
-		
-//comment 
-//testing again
-
-//printing java loving messages
-
-//trying to get conflict deleted
-
-// guess method
- 
 
 	
 
